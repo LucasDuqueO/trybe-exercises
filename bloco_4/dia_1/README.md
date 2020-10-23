0285b653425433d325ebd152cff52bb5caac7745
Lucas Duque <lucas3rios2014@gmail.com>
2020-10-23 19:12:57 -0300

    Exercicios 1 a 4 completos

---
+++ bloco_4/dia_1/README.md
@@ -0,0 +1,2 @@
+Exercicios do bloco 4, dia 1.
+Introdução ao Javascript e Lógica de Programação.
---
+++ bloco_4/dia_1/exercise3.js
@@ -0,0 +1,15 @@
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+
+let a = 321687;
+let b = 754895;
+let c = 478512;
+
+if(a>b && a>c) {
+  console.log(a+" é o maior numero!");
+}
+else if(b>a && b>c) {
+  console.log(b+" é o maior numero!");
+}
+else {
+  console.log(c+" é o maior numero!");
+}
